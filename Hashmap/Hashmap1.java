6fff9a6d29395aa58dce84a507141b41544d2416
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-22 18:21:07 +0530

    Introduction to Hashmaps

---
+++ Hashmap/Hashmap1.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Hashmap1 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Apple");
+        map.put(2, "Mango");
+        map.put(3, "Banana");
+        System.out.println(map);
+        
+    }    
+}
