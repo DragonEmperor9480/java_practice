d33217369399162da6aead0ed0546fea53a09c9d
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-22 16:27:01 +0530

    Array Sorting

---
+++ Arrays/Sorting_arrays.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Sorting_arrays {
+    public static void main(String[] args) {
+        
+    int Numbers[] = {10,6,7,3,1,67,34,23,24,13,543,345,32,1};
+    Arrays.sort(Numbers);
+    System.out.println(Arrays.toString(Numbers));
+    }
+}
