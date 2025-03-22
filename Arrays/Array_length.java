af833c87430b7db9d737c32a33f15a1ee5032664
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-22 16:26:53 +0530

    Array Length

---
+++ Arrays/Array_length.java
@@ -0,0 +1,11 @@
+public class Array_length {
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,5,6};
+
+        for(int i=0;i<num.length;i++)
+        {
+            System.out.println(num[i]);
+        }
+    }
+    
+}
