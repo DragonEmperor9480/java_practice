9af0416bd5f03f68f6f9b76d0dbf83f69c534b42
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-22 18:41:49 +0530

    String Methods part-1

---
+++ Strings/String_methods_1.java
@@ -0,0 +1,18 @@
+public class String_methods_1 {
+    public static void main(String[] args) {
+        String s1 = "Visual Studio Code";
+        //length
+        System.out.println(s1.length());
+        //charAt()
+        System.out.println(s1.charAt(5));
+        //Uppercase
+        System.out.println(s1.toUpperCase());
+        //LowerCase
+        System.out.println(s1.toLowerCase());
+        //concat
+        System.out.println(s1.concat(" Version 1.0"));
+        //Contains
+        System.out.println(s1.contains("vi"));
+
+    }
+}
