630f704f9dcb0eb4dadbdb51c15fd185131b4915
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-22 10:49:59 +0530

    Datatypes

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int Int=1;
+        float Float=4.45f;
+        char Char='a';
+        double Double=6.99;
+        String str="Amrutesh";
+
+        System.out.println("Integer:" + Int + "\nFloat:" + Float + "\nChar:" + Char + "\nDouble:" + Double + "\nString:" + str);
+
+
+    }
+    
+}
